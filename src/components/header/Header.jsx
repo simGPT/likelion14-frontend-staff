add2faba514d27439f0b71ea12d397699ebcffae
simGPT <simGPT02@gmail.com>
2026-04-06 17:31:39 +0900

    :sparkles:Feat: Header UI 작업

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,37 @@
+import styled from "styled-components";
+import logoUrl from "../../assets/images/kream_image.png"
+import homeUrl from "../../assets/icons/home_icon.png"
+
+// 대문자로 시작! -> 대문자를 컨포넌트로 인식하기 때문
+const LogoImage = styled.img`
+    width: 166px;
+    height: 141px;
+`;
+
+const HomeIcon = styled.img`
+    width: 61px;
+    height: 24px;
+`;
+
+const HeaderContainer = styled.div`
+    padding-right: 160px;
+    padding-left: 160px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    
+`;
+
 export default function Header(){
+
     return(
-        <div></div>
+        <div>
+            <HeaderContainer>
+                <LogoImage src={logoUrl}/>
+                <HomeIcon src={homeUrl}/>
+            </HeaderContainer>
+            
+        </div>
     );
 }
+
