84d377260e5e09ac5945f86038d69d2aa8829714
simGPT <simGPT02@gmail.com>
2026-04-10 23:35:01 +0900

    :sparkles:Feat: 상품등록 페이지 라우트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
       <Routes>
         <Route element={<RootLayout />}>
           <Route path="/" element={<Main />} />
+          <Route path="/add" element={<></>}/> {/* 추후 element에 상품 등록 페이지 들어가야함 */}
         </Route>
       </Routes>
     </BrowserRouter>
