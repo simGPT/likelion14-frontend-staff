38f29e234580c9b7b4f1cb44ddf28cf714b2c478
simGPT <simGPT02@gmail.com>
2026-04-10 23:34:23 +0900

    :sparkles:Feat: 상품등록 버튼 추가

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import logoUrl from "../../assets/images/kream_image.png"
 import homeUrl from "../../assets/icons/home_icon.png"
+import {useLocation, useNavigate} from "react-router-dom";
 
 // 대문자로 시작! -> 대문자를 컨포넌트로 인식하기 때문
 const LogoImage = styled.img`
@@ -22,15 +23,38 @@ const HeaderContainer = styled.div`
     
 `;
 
+const Button = styled.div`
+    color: #6C6C6C;
+    font-size: 13px;
+    font-family: Pretendard;
+    font-weight: 400;
+`;
+
+const HeaderRight = styled.div`
+    flex-direction: column;
+    justify-content: flex-start;
+    display: inline-flex;
+    align-items: flex-end;
+    gap: 36px;
+`;
+
 export default function Header(){
+
+    const {pathname} = useLocation(); // 현재 페이지 경로 불러오기
+    const navigate = useNavigate();
+    const buttonName = "상품등록";
 
     return(
         <div>
             <HeaderContainer>
                 <LogoImage src={logoUrl}/>
-                <HomeIcon src={homeUrl}/>
+                <HeaderRight>
+                    {pathname === "/" && (
+                        <Button onClick={()=>navigate("/add")}>{buttonName}</Button>
+                    )}
+                    <HomeIcon src={homeUrl}/>
+                </HeaderRight>
             </HeaderContainer>
-            
         </div>
     );
 }
